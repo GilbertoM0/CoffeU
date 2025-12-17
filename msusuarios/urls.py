cd92a4a30d24b578cf726eb79c1948216f33a6b9
ELBishoCR7 <saulnovoavaldovinos@gmail.com>
2025-12-16 22:24:17 -0600

    Fix merge conflicts

--- msusuarios/urls.py
+++ msusuarios/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     # path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path('products/', include('products.urls')),
-<<<<<<< HEAD
     
-=======
+
     path('ventas/', include('ventas.urls')),
     path('orders/', include('orders.urls')),
->>>>>>> 4b13c376906265c0c77a995e4eec7d8eb069b8b7
 ]
+
